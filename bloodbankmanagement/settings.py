e469dd44bb3f4f1ff0c1fd6f15ce8c50ca1adf85
Chinmay J <120739917+Hades1710@users.noreply.github.com>
2025-03-29 13:38:05 +0530

    Update settings.py

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -84,10 +84,10 @@ WSGI_APPLICATION = 'bloodbankmanagement.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
